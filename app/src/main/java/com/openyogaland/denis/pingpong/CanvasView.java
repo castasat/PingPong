df16eb2805b4166519e69e03f0ed5d52212ddc56
castasat <castasat@mail.ru>
2018-01-22 23:00:18 +0300

    - CanvasView extends View not ViewCompat and implements ICanvasView (View in MVC) - added GameController class (Controller in MVC) - added IGameConstantsModel interface (Model in MVC) - view is opened on full screen, title and action bar are hidded - added showMessage() from ICanvasView to CanvasView - added redraw() from ICanvasView to CanvasView - where will be drawCircle() and drawRectangle() in CanvasView from ICanvasView - fixed package error in activity_main.xml - set screenOrientation to always landscape in Activity section of AndroidManifest.xml

--- app/src/main/java/com/openyogaland/denis/pingpong/CanvasView.java
+++ app/src/main/java/com/openyogaland/denis/pingpong/CanvasView.java
@@ -1,11 +1,19 @@
 package com.openyogaland.denis.pingpong;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.support.v4.view.ViewCompat;
+import android.graphics.Paint.Style;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 //this class is a View, on which we can draw something
-public class CanvasView extends ViewCompat implements ICanvasView
+public class CanvasView extends View implements ICanvasView
 {
   // константы
   private static int screenWidth;  // ширина экрана
@@ -14,4 +22,72 @@ public class CanvasView extends ViewCompat implements ICanvasView
   // поля
   private Paint  paint;  // "кисточка" для рисования
   private Canvas canvas; // "холст" для рисования
+  
+  private GameController gameController; // "логика" игры
+  private Toast          toast;          // всплывающее сообщение
+  
+  // constructor
+  public CanvasView(Context context, AttributeSet attrs)
+  {
+    super(context, attrs);
+    initWidthAndHeight(context); // инициализируем ширину и высоту экрана
+    initPaint();                 // инициализируем "кисточку"
+    gameController = new GameController(this, screenWidth, screenHeight);
+  }
+  
+  // инициализируем ширину и высоту экрана
+  private void initWidthAndHeight(Context context)
+  {
+    WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+    if(windowManager != null)
+    {
+      Display display = windowManager.getDefaultDisplay();
+      Point   point   = new Point();
+      display.getSize(point); // точка приобретает координаты правой нижней точки экрана
+      screenWidth  = point.x;
+      screenHeight = point.y;
+    }
+  }
+  
+  // инициализируем "кисточку"
+  private void initPaint()
+  {
+    paint = new Paint();
+    paint.setAntiAlias(true);   // сглаживание
+    paint.setStyle(Style.FILL); // заливка
+  }
+  
+  // метод интерфейса ICanvasView
+  @Override
+  public void showMessage(String text)
+  {
+    if(toast != null) // если всплывающее сообщение на экране
+    {
+      toast.cancel();
+    }
+    toast = Toast.makeText(getContext(), text, Toast.LENGTH_SHORT);
+    toast.setGravity(Gravity.CENTER, 0, 0);
+    toast.show();
+  }
+  
+  // метод интерфейса ICanvasView
+  @Override
+  public void redraw()
+  {
+    invalidate();
+  }
+  
+  // метод интерфейса ICanvasView
+  @Override
+  public void drawCircle()
+  {
+  
+  }
+  
+  // метод интерфейса ICanvasView
+  @Override
+  public void drawRectahgle()
+  {
+  
+  }
 }
--- app/src/main/java/com/openyogaland/denis/pingpong/ICanvasView.java
+++ app/src/main/java/com/openyogaland/denis/pingpong/ICanvasView.java
@@ -3,4 +3,11 @@ package com.openyogaland.denis.pingpong;
 // Interface to work with from GameManager
 interface ICanvasView
 {
+  void drawCircle();
+  
+  void drawRectahgle();
+  
+  void showMessage(String text);
+  
+  void redraw();
 }
